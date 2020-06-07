a02a77aa880be7f8eca4b834d2e72848a5dcdddd
Tomato <gamerx6900@gmail.com>
2020-06-07 12:23:55 +0300

    build json def updateItem done 27min

---
+++ ecommerce/store/views.py
@@ -0,0 +1,50 @@
+# from django.core.serializers import json
+from django.shortcuts import render
+from django.http import JsonResponse
+import json
+from .models import*
+# Create your views here.
+
+def store(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'store/Store.html', context)
+
+
+def cart(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer= customer, complete= False)
+        items = order.orderitem_set.all()
+    else:
+        items= []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context= {'items': items, 'order': order}
+    return render(request, 'store/Cart.html', context)
+
+
+def checkout(request):
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer= customer, complete= False)
+        items = order.orderitem_set.all()
+    else:
+        items= []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context= {'items': items, 'order': order}
+    return render(request, 'store/Checkout.html', context)
+
+def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action:', action)
+    print('Product:', productId)
+
+
+    return JsonResponse('Item was added', safe=False)
